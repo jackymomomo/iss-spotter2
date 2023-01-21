36431a99ee2d1bee35b15d296a83fb2576cba8c6
jackymomomo <jacklloydrobinson@gmail.com>
2023-01-21 03:14:21 -0800

    function implemented, we can create the third function for our next assignment Fetch the next ISS flyovers for our geo coordinates, API calls implemented, we can now glue or "chain" these together in order to build the app itself. Well do that as the next thing.command-line application, we not only further practiced writing asynchronous tasks using callbacks, we also started seeing the complexity that can come with trying to perform asynchronous tasks in a synchronous way

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+const { fetchCoordsByIP, fetchIP, fetchISSFlyoverTimes, nextISSTimesForLocation } = require('./iss');
+
 fetchIP((error, ip) => {
   if (error) {
     console.log("JS NOT ACCEPTED", error);
@@ -6,6 +9,41 @@ fetchIP((error, ip) => {
 });
 
 
+fetchCoordsByIP('207.216.152.109', (error, data) => {
+  if (error) {
+    console.log("There was an Error", error.message);
+  } else {
+    console.log('you coordinates are', data);
+  }
+});
 
 
+const newCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+
+fetchISSFlyoverTimes(newCoords, (error, passes) => {
+  if (error) {
+    console.log("There was an Error", error.message);
+    return;
+  }
+  console.log(`It worked! returned flyover times ${passes}`);
+});
+
+const printPassTimes = function(passes) {
+  for (let pass = 0; pass > passes; pass++) {
+
+  
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  printPassTimes(passTimes);
+});
 
